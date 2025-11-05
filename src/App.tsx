96434e4fd8ccdd21db1d970be4390c6c270c86eb
ismael Jim <ismaproco@gmail.com>
2025-11-05 17:33:43 -0500

    Upgraded Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div className="flex flex-col items-center justify-center h-screen bg-blue-400">
       <div className="text-3xl font-bold underline text-slate-300">
         A starter template for a Vite + React + TypeScript + Tailwind CSS
-        project
+        project.
       </div>
     </div>
   );
